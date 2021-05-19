52ea8469d5c255386929f6ffe914fd74ba57e3c6
Raghvender <raghvender002@gmail.com>
2021-05-19 12:42:35 +0530

    Script Added

--- script.js
+++ script.js
@@ -1,27 +1,88 @@
 const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({
+    product:'chrome',
+    executablePath: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
     headless: false,
     devtools: true,
+    defaultViewport: null,
   });
   const page = await browser.newPage();
-  page.setViewport({
-    width: 1000,
-    height: 1000,
-    deviceScaleFactor: 1,
-  });
-  await page.goto("http://localhost:4200/#/dashboard", {
+  await page.goto("http://localhost:4200/#/login", {
     waitUntil: "networkidle2",
   });
 
-  // scroll up and down, click on random links
-  await page.keyboard.press("ArrowDown");
-  await page.waitForTimeout(50000);
-  await page.keyboard.up("ArrowDown");
-  await page.waitForTimeout(50000);
-  await page.keyboard.press("ArrowUp");
-  await page.waitForTimeout(50000);
-  await page.keyboard.up("ArrowUp");
+  const emailSelector = '#email'
+  await page.waitForSelector(emailSelector)
+  await page.focus(emailSelector)
+  await page.keyboard.type('tester@yopmail.com',{delay: 100})
+
+  const passwordSelector = '#password'
+  await page.waitForSelector(passwordSelector)
+  await page.focus(passwordSelector)
+  await page.keyboard.type('12345678',{delay: 100})
+
+  const buttonSelector = '#loginBtn'
+  await page.waitForSelector(buttonSelector)
+  await page.click(buttonSelector)
+
+  await page.waitForTimeout(5000)
+  await autoScroll(page)
+  await page.waitForTimeout(5000)
+
+  const userSelector = 'a[href="#/pages/users"]'
+  const vendorSelector = 'a[href="#/pages/vendors"]'
+  const orderSelector = 'a[href="#/pages/orderlist"]'
+
+  while(true){
+    await page.waitForSelector(userSelector)
+    await page.click(userSelector)
+  
+    await page.waitForTimeout(5000)
+    await autoScroll(page)
+    await page.waitForTimeout(5000)
+  
+    await page.waitForSelector(vendorSelector)
+    await page.click(vendorSelector)
+  
+    await page.waitForTimeout(5000)
+    await autoScroll(page)
+    await page.waitForTimeout(5000)
+  
+    await page.waitForSelector(orderSelector)
+    await page.click(orderSelector)
+  
+    await page.waitForTimeout(5000)
+    await autoScroll(page)
+    await page.waitForTimeout(5000)
+  
+    await page.waitForSelector(userSelector)
+    await page.click(userSelector)
+  
+    await page.waitForTimeout(5000)
+    await autoScroll(page)
+    await page.waitForTimeout(5000)
+  }
+  
 
   browser.close();
 })();
+
+async function autoScroll(page){
+  await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+          var totalHeight = 0;
+          var distance = 100;
+          var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if(totalHeight >= scrollHeight){
+                  clearInterval(timer);
+                  resolve();
+              }
+          }, 100);
+      });
+  });
+}
